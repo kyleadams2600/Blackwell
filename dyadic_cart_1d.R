3ac422b4a87ca05df37d301f31bf91eaaf38eacb
Kyle Adams <68289598+kyleadams2600@users.noreply.github.com>
2021-05-30 16:57:15 -0400

    playing around with the theta vector function in the dyadic_cart_1d script, doesn't work atm but wanted to push anyway

--- dyadic_cart_1d.R
+++ dyadic_cart_1d.R
@@ -128,39 +128,25 @@ mse = function(iter){
 }
   ####################################
   
-#####Algorithm for Two-Fold Cross Validation----
-#Step 1: Take odd observations from list of data points
-#Step 2: Create a new data vector of these that looks like y1, y1+y3/2, y3, ...
-#Step 3: Apply dyadic CART algorithm w grid of lambda in {1, 2 , 2^2, ..., 2^(log(n))}
-#Step 4: Now we should have log(n) theta hats each with a different lambda
-#Step 5: Now we want to minimize "prediction error" = [sum of even observations](yi - theta hat, lambdaj,i)
-#Step 6: Now the final fit for the even observations is going to be theta hat (lambda hat odd) @ even observations
-#Step 7: Reverse even and odd
-#Step 8: Combine thetas to have estimate
 
+#playing with dyadic cart----
+#step 1: run code
+#step 2: set l, n
 
-# step 1 + step 2 
-
-crossval = function(y) { # y is the list of observations
+#function that spits out theta values for each lambda
+create_grid = function(n, l, y) { #n is number of lambdas, l is used for n = 2^l in dyadic_1d, y is function
   
-  n = length(y)
-  y_odd = vector(mode = "integer", length = n) # list of odd indexed observations from y
+  lambda_grid = vector(mode = "integer", length = n)
+  theta_vector = list()
+  
+  for (i in 0:n-1) {
+    lambda_grid[i+1] = 2^i #adds power of 2 as a lambda value in grid
+  }
   
   for (i in 1:n) {
-    if (i %% 2 != 0) { # odd indexes
-      y_odd[i] = y[i]
-    } 
-    else if (i %% 2 == 0 && i != n) { # even indexes
-      y_odd[i] == ((y[i-1] + y[i+1]))/2# fill in missing observations with average of neighboring observations
-      message(y[i-1], y[i], y[i+1])
-      message((y[i-1] + y[i+1])/2)
-    } 
-    else if (i == n && i %% 2 == 0) { #if last entry and even length, make last entry an average of the first and second to last observation
-      y_odd[i] == (y[1] + y[i-1])/2
-    }
+    theta_vector[i] = dyadic_1d(l, y, lambda_grid[i]) #creates vector of theta values
   }
-  return(y_odd)
-} 
-
-
-
+  
+  return(theta_vector)
+  
+}
